bc0adcba480fde07b878a1a48dab0ce6112e7e02
Robin Kelsick II <robinkelsick@gmail.com>
2023-08-10 15:13:38 -0400

    done with the hard part. may need to add some stuff later.

--- src/components/Work.js
+++ src/components/Work.js
@@ -12,7 +12,13 @@ const Work = () => {
     <div className="section" id="work">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
-          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+          >
             <div>
               <h2 className="h2 leading-tight text-accent">
                 My Latest <br /> Work
@@ -39,13 +45,19 @@ const Work = () => {
                 <span className="text-3xl text-white">Project Name</span>
               </div>
             </div>
-          </div>
-          <div className="flex-1 flex flex-col gap-y-12 mt-24">
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex flex-col gap-y-12 mt-24"
+          >
             <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               <img
                 className="group-hover:scale-125 transition-all duration-500"
-                src={img1}
+                src={img2}
                 alt=""
               />
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
@@ -59,7 +71,7 @@ const Work = () => {
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               <img
                 className="group-hover:scale-125 transition-all duration-500"
-                src={img1}
+                src={img3}
                 alt=""
               />
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
@@ -69,7 +81,7 @@ const Work = () => {
                 <span className="text-3xl text-white">Project Name</span>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
